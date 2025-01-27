543991a68e8f7c60c00e717d2f48543f996c8960
Павел Зуев <tgr7362@gmail.com>
2025-01-27 22:53:25 +0300

    auth error

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -43,6 +43,7 @@ class CustomCreateUserSerializer(ModelSerializer):
     class Meta:
         model = User
         fields = ('email', 'username', 'first_name', 'last_name', 'password')
+        extra_kwargs = {'password': {'write_only': True}}
 
     def create(self, validated_data):
         user = User(
