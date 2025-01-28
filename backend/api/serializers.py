f05b79f10ee7a41602496d782455a6c9c4425c71
Павел Зуев <tgr7362@gmail.com>
2025-01-28 21:45:21 +0300

    change create user serializer

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -46,15 +46,7 @@ class CustomCreateUserSerializer(ModelSerializer):
         extra_kwargs = {'password': {'write_only': True}}
 
     def create(self, validated_data):
-        user = User(
-            email=validated_data['email'],
-            username=validated_data['username'],
-            first_name=validated_data['first_name'],
-            last_name=validated_data['last_name'],
-        )
-        user.set_password(validated_data['password'])
-        user.save()
-        return user
+        return User.objects.create_user(**validated_data)
 
 
 class RecipeSerializer(ModelSerializer):
